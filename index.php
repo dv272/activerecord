4687d7e028e343a8a25230f7b8749e54f5a3a9df
dwija vishvesh vyas <dv272@afsconnect1.njit.edu>
2017-11-16 20:28:04 -0500

    select one record added

--- index.php
+++ index.php
@@ -22,7 +22,16 @@
 		}
 		echo "</table><hr>";
 		
-	
+		$id = 3;
+		$record = todos::findOne($id);
+		echo '<h2>Select One Record</h2>';
+		echo '<p>Select Record ID: '.$id.'</p>';
+		echo "<table>";
+		echo "<tr><th>TODO ID</th><th>Owner Email</th><th>Owner ID</th><th>Create Date</th><th>Due Date</th><th>Message</th><th>Is Done?</th></tr>";
+		echo "<tr><td>".$record->id."</td><td>".$record->owneremail."</td><td>".$record->ownerid."</td><td>".$record->createddate."</td><td>".$record->duedate."</td><td>".$record->message."</td><td>".$record->isdone."</td></tr>";
+			
+		echo "</table><hr>";
+		
 	?>
 </body>
 </html>
