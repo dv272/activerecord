404eb77533041198cbddf8984424c4360431219f
dwija vishvesh vyas <dv272@afsconnect1.njit.edu>
2017-11-16 20:47:25 -0500

    Insert added to index.php

--- index.php
+++ index.php
@@ -32,6 +32,29 @@
 			
 		echo "</table><hr>";
 		
+		echo '<h2>Insert/Update New Record</h2>';
+		$td = new todo();
+		$td->id = 8;
+		$td->owneremail = 'mjlee@njit.edu';
+		$td->ownerid = 1;
+		$td->createddate = '2017-11-15 12:34:45';
+		$td->duedate = '2017-11-25 12:34:45';
+		$td->message = 'Demo task 1';
+		$td->isdone = 0;
+		$td->save();
+		echo '<p>Todo Record data: Title = '.$td->message.', Status = '.$td->isdone.'</p>';
+		echo "<table>";
+		$recordAll = todos::findAll();
+		if (count($recordAll) > 0) {
+			echo "<tr><th>ToDo ID</th><th>Owner Email</th><th>Owner ID</th><th>Create Date
+			</th><th>Due Date</th><th>Message</th><th>Is Done?</th></tr>";
+			foreach ($recordAll as $row) {
+				echo "<tr><td>".$row->id."</td><td>".$row->owneremail."</td><td>".$row->ownerid."</td><td>".$row->createddate."</td>
+				<td>".$row->duedate."</td><td>".$row->message."</td><td>".$row->isdone."</td></tr>";
+			}
+		}
+		echo '</table><br><hr>';
+		
 	?>
 </body>
 </html>
